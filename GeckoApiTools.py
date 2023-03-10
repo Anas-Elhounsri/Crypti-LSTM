8ca1a5cfa71ab732221cdb76d4ee5a27ab441f8c
Anas-Elhounsri <anas.el.games@gmail.com>
2023-03-10 11:32:26 +0300

    Made syntax changes in s3.put_object nothing major

--- GeckoApiTools.py
+++ GeckoApiTools.py
@@ -1,25 +1,12 @@
+import json
 from pycoingecko import CoinGeckoAPI
 import  boto3
 import csv
 
 cg = CoinGeckoAPI()
-s3 = boto3.client('s3')#defines the client to be s3
 
 def getCoinsList():
     return cg.getCoinsList()
-
-def getCoinData(coin_name, fiat_currency="usd", days="max"):
-    coin_market_data = cg.get_coin_market_chart_by_id(
-        coin_name,
-        fiat_currency,
-        days,
-    )
-
-    prices = coin_market_data["prices"]
-    market_caps = coin_market_data["market_caps"]
-    volumes = coin_market_data["total_volumes"]
-
-    return prices, market_caps, volumes
 
 #this is the lamda function that receives trigger events 
 #(now its manual i will automate the process with API GateWay HTTP request)
@@ -38,15 +25,35 @@ def lambda_handler(event, context):
     #Creates a CSV file and fills it with data with for loop
     csv_string =""
     for i in data:
-        #string concatination ik fancy stuff ;)
+        #ik fancy stuff ;)
         csv_string += f"{i[0]}, { i[1]}, {i[2]} \n"
 
     #this saves the CSV data into the s3
-    bucket_name = "crypti-food"
-    object_key = "coin-data/bitcoin.csv"
-    s3.put_object(Bucket=bucket_name, Key=object_key, Body=csv_string)
+    s3 = boto3.client('s3')#defines the client to be s3
+    # bucket_name = "crypti-food"
+    # object_key = "coin-data/bitcoin.csv"
+    # s3.put_object(Bucket=bucket_name, Key=object_key, Body=csv_string)
+    s3 = boto3.client('s3')
+    s3.put_object(
+        Bucket='crypti-food',
+        Key='coin-market-data/{}.csv'.format("bitcoin"),
+        Body=csv_string
+        )
 
     return {
-        'statuCode': 200,
-        'body' : 'Data saved to s3' 
+        "statusCode": 200,
+        "body" : "Data saved to s3"
     }
+
+def getCoinData(coin_name, fiat_currency="usd", days="max"):
+    coin_market_data = cg.get_coin_market_chart_by_id(
+        coin_name,
+        fiat_currency,
+        days,
+    )
+
+    prices = coin_market_data["prices"]
+    market_caps = coin_market_data["market_caps"]
+    volumes = coin_market_data["total_volumes"]
+
+    return prices, market_caps, volumes
