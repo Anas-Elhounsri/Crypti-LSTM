c4ff285f42cbf191c08d5bb0efb7255b4f46c67f
Anas-Elhounsri <anas.el.games@gmail.com>
2023-06-18 15:20:40 +0300

    Masked sensitive data

--- ec2_on.py
+++ ec2_on.py
@@ -1,7 +1,7 @@
 import boto3
 import json
-region = '<region name>'
-instances = '[<Intance id>]'
+region = '<region-name>'
+instances = '[<Intance-id>]'
 ec2 = boto3.client('ec2', region_name=region)
 
 def lambda_handler(event, context):
